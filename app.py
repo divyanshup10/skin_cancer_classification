31284608de70b3713ab9d6fc39b762c5a2cbfce4
divyanshup10 <154598546+divyanshup10@users.noreply.github.com>
2025-03-22 18:55:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ import numpy as np
 app = Flask(__name__)
 
 # Load your saved model
-model = load_model('skin_new_model.h5',compile=False)
+# model = load_model('skin_new_model.h5',compile=False)
+
+model = load_model('skin_new_model.h5', custom_objects={'InputLayer': None}, compile=False)
 
 # Define class labels based on your dataset
 CLASS_LABELS = {
